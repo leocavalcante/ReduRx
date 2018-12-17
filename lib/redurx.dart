79654ebee25396be27a4106279c48be144b999e5
leocavalcante <lc@leocavalcante.com>
2018-12-17 14:47:22 -0200

    Stable and documented

--- lib/redurx.dart
+++ lib/redurx.dart
@@ -1,3 +1,4 @@
+/// 👌 A thin layer of a Redux-based state manager on top of RxDart.
 library redurx;
 
 import 'dart:async';
@@ -6,34 +7,55 @@ import 'package:rxdart/rxdart.dart';
 
 export 'src/log_middleware.dart';
 
+/// Base interface for all action types.
 abstract class ActionType {}
 
+/// Action for synchronous requests.
 abstract class Action<T> implements ActionType {
+  /// Method to perform a synchronous mutation on the state.
   T reduce(T state);
 }
 
+/// Reducer function type for state mutations.
 typedef T Computation<T>(T state);
 
+/// Action for asynchronous requests.
 abstract class AsyncAction<T> implements ActionType {
+  /// Method to perform a asynchronous mutation on the state.
   Future<Computation<T>> reduce(T state);
 }
 
+/// Interface for Middlewares.
 abstract class Middleware<T> {
+  /// Called before action reducer.
   T beforeAction(ActionType action, T state) => state;
+
+  /// Called after action reducer.
   T afterAction(ActionType action, T state) => state;
 }
 
+/// The heart of the idea, this is where we control the State and dispatch Actions.
 class Store<T> {
+  /// You can create the Store given an [initialState].
   Store([T initialState])
       : subject = BehaviorSubject<T>(seedValue: initialState);
 
+  /// This is where RxDart comes in, we manage the final state using a [BehaviorSubject].
   final BehaviorSubject<T> subject;
+
+  /// List of middlewares to be applied.
   final List<Middleware<T>> middlewares = [];
 
+  /// Gets the subject stream.
   Stream<T> get stream => subject.stream;
+
+  /// Gets the subject current value/store's current state.
   T get state => subject.value;
+
+  /// Maps the current subject stream to a new Stream.
   Stream<S> map<S>(S convert(T state)) => stream.map(convert);
 
+  /// Dispatches actions that mutates the current state.
   Store<T> dispatch(ActionType action) {
     if (action is Action<T>) {
       final afterAction =
@@ -57,11 +79,13 @@ class Store<T> {
     return this;
   }
 
+  /// Adds middlewares to the store.
   Store<T> add(Middleware<T> middleware) {
     middlewares.add(middleware);
     return this;
   }
 
+  /// Closes the stores subject.
   void close() => subject.close();
 
   T _computeBeforeMiddlewares(ActionType action, T state) =>
--- lib/src/log_middleware.dart
+++ lib/src/log_middleware.dart
@@ -1,12 +1,15 @@
 import '../redurx.dart';
 
+/// Middleware that prints when action's reducers are applied.
 class LogMiddleware<T> extends Middleware<T> {
+  /// Prints before the Action reducer call.
   @override
   T beforeAction(action, state) {
     print('Before action: ${action.runtimeType}: $state');
     return super.beforeAction(action, state);
   }
 
+  /// Prints after the Action reducer call.
   @override
   T afterAction(action, state) {
     print('After action: ${action.runtimeType}: $state');
