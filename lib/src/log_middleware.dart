e75e4c366708c52aef0ef05903318d671b3b7814
leocavalcante <lc@leocavalcante.com>
2019-02-09 01:26:31 -0200

    Middlewares knows about the Store

--- lib/src/log_middleware.dart
+++ lib/src/log_middleware.dart
@@ -4,15 +4,15 @@ import '../redurx.dart';
 class LogMiddleware<T> extends Middleware<T> {
   /// Prints before the Action reducer call.
   @override
-  T beforeAction(action, state) {
+  T beforeAction(store, action, state) {
     print('Before action: ${action.runtimeType}: $state');
-    return super.beforeAction(action, state);
+    return super.beforeAction(store, action, state);
   }
 
   /// Prints after the Action reducer call.
   @override
-  T afterAction(action, state) {
+  T afterAction(store, action, state) {
     print('After action: ${action.runtimeType}: $state');
-    return super.afterAction(action, state);
+    return super.afterAction(store, action, state);
   }
 }
